082890214843bdaed5aae9aff3e2a3e4143926a3
inconveniencestore <iivyleaves@gmail.com>
2024-12-16 12:30:26 -0500

    Update main.js

--- robert/JS/main.js
+++ robert/JS/main.js
@@ -1,1 +1,11 @@
 import "../CSS/style.css";
+
+const DOMSelectors = {
+  startButton: document.querySelector("start-button"),
+};
+
+DOMSelectors.startButton.addEventListener("click", initGame);
+
+initGame() {
+    
+}
