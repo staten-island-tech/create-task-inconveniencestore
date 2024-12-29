2cb5e4638f702a79f78bcf6080fafd36e358aa55
inconveniencestore <iivyleaves@gmail.com>
2024-12-29 17:17:21 -0500

    a start is a start

--- robert/JS/main.js
+++ robert/JS/main.js
@@ -2,8 +2,32 @@ import "../CSS/style.css";
 
 const DOMSelectors = {
   startButton: document.querySelector("start-button"),
+  timerArea: document.querySelector("timer-area"),
 };
 
-DOMSelectors.startButton.addEventListener("click", initGame);
+//DOMSelectors.startButton.addEventListener("click", initGame);
 
-function initGame() {}
+let duration = 10;
+
+//function initGame() {}
+
+function wait(ms) {
+  //when resolve is called, it counts as promise fulfilled
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function updateCountdownDisplay() {
+  DOMSelectors.timerArea.textContent = `time remaining: ${duration}s`;
+}
+
+async function countdown() {
+  while (duration > 0) {
+    updateCountdownDisplay();
+    await wait(1000);
+    duration--;
+  }
+
+  DOMSelectors.timerArea.textContent = `TIMES UP`;
+}
+
+countdown();
