d5efd3a760ba03bce454ec63110242e12715f3a6
inconveniencestore <iivyleaves@gmail.com>
2025-01-02 12:30:16 -0500

    lalalallala

--- robert/JS/items_for_sale.js
+++ robert/JS/items_for_sale.js
@@ -1,7 +1,66 @@
 const normalAuctionItems = [
   {
-    name: "demo",
-    description: "description",
+    name: "banana",
+    description:
+      "it could be the one that's taped on the wall for 10 million dollars or the walmart banana for like a dollar",
+    image: "",
+
+    minIncrease: 5,
+    value: 20,
+    bidEagerness: 20,
+    audienceBidRange: [5, 10], //what did this mean
+    //specialBidderInclusion: "array name here in bidders",
+  },
+  {
+    name: "one dollar",
+    description: "thats right, one freedom dollar",
+    image: "",
+
+    minIncrease: 0.01,
+    value: 1,
+    bidEagerness: 20,
+    audienceBidRange: [5, 10], //what did this mean
+    //specialBidderInclusion: "array name here in bidders",
+  },
+  {
+    name: "one canadian geese",
+    description: "thats right, one freedom dollar",
+    image: "",
+
+    minIncrease: 0.01,
+    value: 1,
+    bidEagerness: 20,
+    audienceBidRange: [5, 10], //what did this mean
+    //specialBidderInclusion: "array name here in bidders",
+  },
+];
+
+const highEndAuctionItems = [
+  {
+    name: "australia",
+    description: "yeah, thats right, the whole country",
+    image: "",
+
+    minIncrease: 5,
+    value: 20,
+    bidEagerness: 20,
+    audienceBidRange: [5, 10], //what did this mean
+    //specialBidderInclusion: "array name here in bidders",
+  },
+  {
+    name: "panama canal",
+    description: "??? why is this here",
+    image: "",
+
+    minIncrease: 5,
+    value: 20,
+    bidEagerness: 20,
+    audienceBidRange: [5, 10], //what did this mean
+    //specialBidderInclusion: "array name here in bidders",
+  },
+  {
+    name: "canada",
+    description: "AMERICAAAAAAAAAAAAAAAAAAAAAAAAAA",
     image: "",
 
     minIncrease: 5,
@@ -11,7 +70,5 @@ const normalAuctionItems = [
     //specialBidderInclusion: "array name here in bidders",
   },
 ];
-
-const highEndAuctionItems = [];
 
 export { normalAuctionItems };
